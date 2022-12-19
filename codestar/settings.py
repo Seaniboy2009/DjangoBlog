6b76fa71dd0a644bf97f972e5fb2a3507c915236
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-12-19 13:22:27 +0000

    fixed allow_host url

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sean-clark-django-blog.heroku.com']
+ALLOWED_HOSTS = ['sean-clark-django-blog.herokuapp.com']
 
 
 # Application definition
